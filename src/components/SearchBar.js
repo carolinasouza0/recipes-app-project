655fc7a676dc2f1e54686d6415599f38c15465fb
Bruno <bruno_bury@hotmail.com>
2023-07-10 22:42:24 -0300

    requisitos 10 e 11 resolvidos

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,64 @@
 import PropTypes from 'prop-types';
-// import { fetchApiMeals, fetchApiDrinks } from '../utils/FetchAPI';
+import { useContext /* useEffect */ } from 'react';
+import { fetchApiMeals /* fetchApiDrinks */ } from '../utils/FetchAPI';
+// import { useHistory } from 'react-router-dom';
+import RecipesContext from '../context/RecipesContext';
 
 function SearchBar({ showSearchBar }) {
+//   const history = useHistory();
+  const {
+    searchType,
+    setSearchType,
+    searchInput,
+    setSearchInput,
+    setRecipes,
+  } = useContext(RecipesContext);
+
+  //   useEffect(() => {
+  //     if (history.location.pathname === '/meals') setRoutes('meal');
+  //     if (history.location.pathname === '/drinks') setRoutes('drinks');
+  //   });
+
+  const getSearchAPI = async () => {
+    if (searchType === 'first-letter' && searchInput.length !== 1) {
+      global.alert('Your search must have only 1 (one) character');
+      return;
+    }
+
+    if (
+      searchType === 'ingredient'
+      || (searchType === 'name')
+      || (searchType === 'first-letter' && searchInput.length === 1)
+    ) {
+      const newRecipes = await fetchApiMeals(searchInput, searchType);
+      setRecipes(newRecipes);
+    } else {
+      global.alert('Invalid search type');
+    }
+  };
+
   return (
     <div>
       {showSearchBar && (
         <div>
           <input
             type="text"
+            name="search-input"
+            value={ searchInput }
+            onChange={ ({ target: { value } }) => setSearchInput(value) }
             data-testid="search-input"
           />
           <div>
             <label>
               <input
                 type="radio"
+                name="searchType"
                 value="ingredient"
+                // checked={ searchType === 'ingredient' }
+                onChange={ ({ target: { value } }) => setSearchType(value) }
                 data-testid="ingredient-search-radio"
               />
-              Search by Ingredient
-            </label>
-          </div>
-
-          <div>
-            <label>
-              <input type="radio" value="name" data-testid="name-search-radio" />
-              Search by Name
+              Ingredient
             </label>
           </div>
 
@@ -32,14 +66,33 @@ function SearchBar({ showSearchBar }) {
             <label>
               <input
                 type="radio"
-                value="first-letter"
-                data-testid="first-letter-search-radio"
+                name="searchType"
+                value="name"
+                // checked={ searchType === 'name' }
+                onChange={ ({ target: { value } }) => setSearchType(value) }
+                data-testid="name-search-radio"
               />
-              Search by First Letter
+              Name
             </label>
           </div>
 
-          <button data-testid="exec-search-btn">Search</button>
+          <div>
+            <label>
+              <input
+                type="radio"
+                name="searchType"
+                value="first-letter"
+                // checked={ searchType === 'first-letter' }
+                onChange={ ({ target: { value } }) => setSearchType(value) }
+                data-testid="first-letter-search-radio"
+              />
+              First Letter
+            </label>
+          </div>
+
+          <button onClick={ getSearchAPI } data-testid="exec-search-btn">
+            Search
+          </button>
         </div>
       )}
     </div>
