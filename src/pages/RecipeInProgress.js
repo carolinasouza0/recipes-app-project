9dca5e007fc22bc797c59b0aed616439d04d2d9b
Bruno <bruno_bury@hotmail.com>
2023-07-12 15:12:03 -0300

    requisito 37 inicio tela in-progress

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,7 +1,32 @@
 function RecipeInProgress() {
   return (
     <div>
-      RecipeInProgress
+      <img
+        src="https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg"
+        alt="recipe"
+        data-testid="recipe-photo"
+      />
+      <h1
+        data-testid="recipe-title"
+      >
+        Recipe Name
+      </h1>
+      <button
+        data-testid="share-btn"
+      >
+        Compartilhar
+      </button>
+      <button
+        data-testid="favorite-btn"
+      >
+        Favoritar
+      </button>
+
+      <p data-testid="recipe-category">
+        Categoria:
+      </p>
+      <p data-testid="instructions">recipe.instructions</p>
+      <button data-testid="finish-recipe-btn">Finalizar Receita</button>
     </div>
   );
 }
