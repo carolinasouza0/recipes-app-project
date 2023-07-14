0de8dcc55e4918c2a301786f7d02cd261c41d9e2
Douglas da Cruz <ducatti_cruz@hotmail.com>
2023-07-14 11:48:58 -0300

    requisito 44 ok

--- src/components/BtnCompartilha.js
+++ src/components/BtnCompartilha.js
@@ -1,11 +1,40 @@
-function BtnCompartilhar(index) {
+import PropTypes from 'prop-types';
+import clipboardCopy from 'clipboard-copy';
+import { useState } from 'react';
+import shareImage from '../images/shareIcon.svg';
+
+function BtnCompartilhar({ index, type, idReference }) {
+  const [copyLink, setCopyLink] = useState(false);
+  const recipeType = type === 'meals' ? 'meals' : 'drinks';
+  const copy = clipboardCopy;
+
+  const handleShareClick = () => {
+    setCopyLink(true);
+    const url = `/${recipeType}/${idReference}`;
+    copy(`http://localhost:3000${url}`);
+    console.log(`http://localhost:3000${url}`);
+  };
+
   return (
-    <button
-      data-testid={ `${index}-horizontal-share-btn` }
-    >
-      Compartilhe
-    </button>
+    <div>
+      { copyLink && <p>Link copied!</p> }
+      <button
+        data-testid={ `${index}-horizontal-share-btn` }
+        onClick={ handleShareClick }
+      >
+        <img
+          src={ shareImage }
+          alt="share"
+        />
+      </button>
+    </div>
   );
 }
+
+BtnCompartilhar.propTypes = {
+  idReference: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
+  type: PropTypes.string.isRequired,
+};
 
 export default BtnCompartilhar;
---
+++ src/components/CardDoneDrinks.js
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import BtnCompartilhar from './BtnCompartilha';
+
+function CardDoneDrinks({ item, index }) {
+  const limiteTag = 2;
+  const tags = (strTags) => {
+    if (strTags === null) {
+      return [];
+    }
+    const splitTag = strTags.split(/,/);
+    return splitTag;
+  };
+  console.log(tags(item.strTags));
+  return (
+    <div key={ index }>
+      {
+        ` é comida ${item.strDrink}`
+      }
+      <img
+        data-testid={ `${index}-horizontal-image` }
+        src={ item.strDrinkThumb }
+        alt={ item.strDrinkThumb }
+      />
+      <p
+        data-testid={ `${index}-horizontal-top-text` }
+      >
+        {` ${item.strAlcoholic}`}
+      </p>
+      <p
+        data-testid={ `${index}-horizontal-name` }
+      >
+        { item.strDrink }
+      </p>
+      <p
+        data-testid={ `${index}-horizontal-done-date` }
+      >
+        {/* aqui é a data que foi feita */}
+        { item.dateModified }
+      </p>
+      <BtnCompartilhar index={ index } type="drinks" idReference={ item.idDrink } />
+      {
+        tags(item.strTags).slice(0, limiteTag).map((tag, indexTag) => (
+          <p
+            key={ indexTag }
+            data-testid={ `${index}-${tag}-horizontal-tag` }
+          >
+            { tag }
+          </p>
+        ))
+      }
+    </div>
+  );
+}
+
+CardDoneDrinks.propTypes = {
+  index: PropTypes.number.isRequired,
+  item: PropTypes.shape({
+    dateModified: PropTypes.number,
+    strArea: PropTypes.string,
+    strAlcoholic: PropTypes.string,
+    strDrink: PropTypes.string,
+    strDrinkThumb: PropTypes.string,
+    strTags: PropTypes.string,
+    idDrink: PropTypes.number,
+  }).isRequired,
+};
+
+export default CardDoneDrinks;
